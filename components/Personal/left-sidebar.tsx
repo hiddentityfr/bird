6a5e7da56329aebddf5ed174098e9f11329a6598
Adrien Colombier <33284435+PixDay@users.noreply.github.com>
2020-09-20 23:14:24 +0200

    Apply css rules to left-sidebar component

--- components/Personal/left-sidebar.tsx
+++ components/Personal/left-sidebar.tsx
@@ -2,15 +2,39 @@ import React from 'react';
 
 export default function LeftSideBar() {
   return (
-    <div>
-      <p>Hiddentity logo</p>
-      <p>Hiddentity</p>
-      <p>Accueil</p>
-      <p>Candidats</p>
-      <p>Offres</p>
-      <p>Administration</p>
-      <p>Mon compte</p>
-      <p>Paramètres</p>
+    <div className="left-sidebar">
+      <div className="title-left-sidebar">
+        <img
+          src="/images/logo-color.png"
+          alt="Hiddentity logo"
+          className="img-title-left-sidebar"
+        />
+        <p className="text-title-sidebar">Hiddentity</p>
+      </div>
+
+      <div className="navigation-left-sidebar">
+        <div className="div-nav-left-sidebar">
+          <p className="nav-left-sidebar">Accueil</p>
+        </div>
+        <div className="div-nav-left-sidebar">
+          <p className="nav-left-sidebar">Candidats</p>
+        </div>
+        <div className="div-nav-left-sidebar">
+          <p className="nav-left-sidebar">Offres</p>
+        </div>
+        <div className="div-nav-left-sidebar">
+          <p className="nav-left-sidebar-active">Administration</p>
+        </div>
+      </div>
+
+      <div className="bot-navigation-left-sidebar">
+        <div className="account-navigation-left-sidebar">
+          <p className="text-bot-navigation-left-sidebar">Mon compte</p>
+        </div>
+        <div className="parameters-navigation-left-sidebar">
+          <p className="text-bot-navigation-left-sidebar">Paramètres</p>
+        </div>
+      </div>
     </div>
   );
 }
