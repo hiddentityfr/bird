0eaf507132be40421236c5651ee6eea163c6fa97
Adrien Colombier <33284435+PixDay@users.noreply.github.com>
2020-09-20 21:39:38 +0200

    Fill left-sdebar file with the text

--- components/Personal/left-sidebar.tsx
+++ components/Personal/left-sidebar.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
 export default function LeftSideBar() {
-  return <div>LeftSideBar</div>;
+  return (
+    <div>
+      <p>Hiddentity logo</p>
+      <p>Hiddentity</p>
+      <p>Accueil</p>
+      <p>Candidats</p>
+      <p>Offres</p>
+      <p>Administration</p>
+      <p>Mon compte</p>
+      <p>Paramètres</p>
+    </div>
+  );
 }
