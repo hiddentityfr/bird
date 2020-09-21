823de6bee00fd0a14c563de07be64378f8913bbd
Adrien Colombier <33284435+PixDay@users.noreply.github.com>
2020-09-21 22:53:06 +0200

    Edit administration page using the components

--- pages/administration.tsx
+++ pages/administration.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import Container from '../Components/Layouts/Container';
+import Text from '../components/DataDisplay/Text';
 import LeftSideBar from '../components/Personal/left-sidebar';
 
 export default function Administration() {
   return (
     <div className="div-full-space">
-      <LeftSideBar />
-      <div>Administration</div>
+      <Container row justify="stretch" align="stretch">
+        <LeftSideBar />
+        <Container>
+          <Text variant="h4" color="#000000">
+            Administration
+          </Text>
+        </Container>
+      </Container>
     </div>
   );
 }
