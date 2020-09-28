4155cc8d5330731e448082b3c5bb2468575f7589
Adrien Colombier <Merline@localhost.localdomain>
2020-09-28 15:27:12 +0200

    Create filters list row

--- pages/administration.tsx
+++ pages/administration.tsx
@@ -11,15 +11,33 @@ export default function Administration() {
       <Container row justify="flex-start" align="stretch">
         <LeftSideBar />
         <Container>
-          <Container row justify="space-between">
-            <Text variant="h4" color="#000000">
-              Administration
-            </Text>
-            <TextField
-              size="long"
-              onChange={() => {}}
-              placeholder="Rechercher une équipe..."
-            />
+          <Container justify="flex-start" align="stretch">
+            <Container row justify="space-between">
+              <Text variant="h4" color="#000000">
+                Administration
+              </Text>
+              <TextField
+                size="long"
+                onChange={() => {}}
+                placeholder="Rechercher une équipe..."
+              />
+            </Container>
+            <Container justify="flex-start" align="stretch">
+              <Container row justify="space-between" align="center">
+                <Text variant="h5" color="#000000">
+                  Equipes
+                </Text>
+                <Text variant="h5" color="#bdbdbd">
+                  Rôles
+                </Text>
+                <Text variant="h5" color="#bdbdbd">
+                  Membres
+                </Text>
+                <Text variant="h5" color="#bdbdbd">
+                  Entreprise
+                </Text>
+              </Container>
+            </Container>
           </Container>
         </Container>
       </Container>
