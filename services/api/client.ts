33783db9e5cd55bf40d26179707b8d879e4111f2
Flayac Quentin <quentin1.flayac@epitech.eu>
2021-06-09 07:27:26 +0200

    No form

--- services/api/client.ts
+++ services/api/client.ts
@@ -17,11 +17,11 @@ const authLink = setContext((_, { headers }) => {
 });
 
 const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-    defaultOptions: {
-      watchQuery: { fetchPolicy: 'network-only' },
-    },
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: { fetchPolicy: 'network-only' },
+  },
 });
 
 export default client;
