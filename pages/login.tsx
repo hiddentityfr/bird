5a1bef78324af2fa90d81c880afbccdd04a99731
Yann Sainson <me@yannsainson.com>
2021-06-10 06:41:12 +0200

    Rebase develop

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Router  from 'next/router';
+import Router from 'next/router';
 
 import Container from '@components/Layouts/Container';
 import Text from '@components/DataDisplay/Text';
@@ -11,70 +11,109 @@ import { useMutation } from '@apollo/client';
 import { api } from '@services';
 
 type LoginResponse = {
-    companyLogin: {
-        token: string;
-        refreshToken: string;
-    }
-}
+  companyLogin: {
+    token: string;
+    refreshToken: string;
+  };
+};
 type LoginVars = {
-    input: {
-        email: string;
-        password: string;
-    }
-}
+  input: {
+    email: string;
+    password: string;
+  };
+};
 
 const Login = (): JSX.Element => {
-    const [email, setEmail] = React.useState<string>();
-    const [password, setPassword] = React.useState<string>();
-    const [badUser, setBadUser] = React.useState<boolean>();
-    const [errorMsg, setErrorMsg] = React.useState<string>();
+  const [email, setEmail] = React.useState<string>();
+  const [password, setPassword] = React.useState<string>();
+  const [badUser, setBadUser] = React.useState<boolean>();
+  const [errorMsg, setErrorMsg] = React.useState<string>();
 
-    const handleEmail = (e: string) => { 
-        if (e != null) 
-            setEmail(e) 
+  const handleEmail = (e: string) => {
+    if (e != null) setEmail(e);
+  };
+  const handlePassword = (e: string) => {
+    if (e != null) setPassword(e);
+  };
+  const [login] = useMutation<LoginResponse, LoginVars>(
+    api.user.mutations.companyLogin,
+    {
+      onCompleted: (data) => {
+        // console.log(data.login.token);
+        localStorage.setItem('token', data.companyLogin.token);
+        localStorage.setItem('refreshToken', data.companyLogin.refreshToken);
+        Router.push('/'); // Merge to get Homepage
+      },
+      onError: () => {
+        setErrorMsg('Les identifiants sont incorrects');
+        setBadUser(true);
+      },
     }
-    const handlePassword = (e: string) => {
-        if (e != null)
-            setPassword(e)
-    }
-    const [login] = useMutation<LoginResponse, LoginVars>(api.user.mutations.companyLogin, {
-        onCompleted: data => {
-            // console.log(data.login.token);
-            localStorage.setItem("token", data.companyLogin.token);
-            localStorage.setItem("refreshToken", data.companyLogin.refreshToken);
-            Router.push("/"); // Merge to get Homepage
-        },
-        onError: () => {
-            setErrorMsg("Les identifiants sont incorrects")
-            setBadUser(true);
-        }
+  );
+  const logger = () => () => {
+    login({
+      variables: {
+        input: { email: email as string, password: password as string },
+      },
     });
-    const logger = () => () => {
-        login({ variables: { input: { email: email as string, password: password as string} } });
-    };
-    if (localStorage.getItem("token")) {
-        Router.push("/");
-        // return (
-            // <Container align="center" gap={0}>You are logged in.</Container>
-            // redirect to main page, token saved on localstorage, possibility to get a cookie if needed
-            // )
-    }
-    return (
-        <Container align="center" gap={0}>
-            <Container row align="center">
-                <Logo />
-                <Text variant="h3" color={theme.cvar('colorForeground')}>Hiddentity</Text>
-            </Container>
-            <Container gap={0}>
-                <Text variant="h5" align="left">Adresse mail</Text>
-                <TextField size="long" thickness="large" error={badUser as boolean} onChange={(e) => {handleEmail(e)}} type="text" placeholder="pierre.dupont@example.com" />
-                <Text variant="h5" align="left">Mot de passe</Text>
-                <TextField size="long" thickness="large" error={badUser as boolean} onChange={(e) => {handlePassword(e)}} type="password" placeholder="********" />
-                <Button size="long" variant="teal" thickness="large" onClick={ logger() }>Login</Button>
-                { badUser && <Text variant="h5" align="left" color={theme.cvar('colorSelectTextError')}>{errorMsg}</Text> }
-            </Container>
-        </Container>
-    );
+  };
+  if (localStorage.getItem('token')) {
+    Router.push('/');
+    // return (
+    // <Container align="center" gap={0}>You are logged in.</Container>
+    // redirect to main page, token saved on localstorage, possibility to get a cookie if needed
+    // )
+  }
+  return (
+    <Container align="center" gap={0}>
+      <Container row align="center">
+        <Logo />
+        <Text variant="h3" color={theme.cvar('colorForeground')}>
+          Hiddentity
+        </Text>
+      </Container>
+      <Container gap={0}>
+        <Text variant="h5" align="left">
+          Adresse mail
+        </Text>
+        <TextField
+          size="long"
+          thickness="large"
+          error={badUser as boolean}
+          onChange={(e) => {
+            handleEmail(e);
+          }}
+          type="text"
+          placeholder="pierre.dupont@example.com"
+        />
+        <Text variant="h5" align="left">
+          Mot de passe
+        </Text>
+        <TextField
+          size="long"
+          thickness="large"
+          error={badUser as boolean}
+          onChange={(e) => {
+            handlePassword(e);
+          }}
+          type="password"
+          placeholder="********"
+        />
+        <Button size="long" variant="teal" thickness="large" onClick={logger()}>
+          Login
+        </Button>
+        {badUser && (
+          <Text
+            variant="h5"
+            align="left"
+            color={theme.cvar('colorSelectTextError')}
+          >
+            {errorMsg}
+          </Text>
+        )}
+      </Container>
+    </Container>
+  );
 };
 
 export default Login;
