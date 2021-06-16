2035198369765091102f24c97047cffb84a4bf53
Yann Sainson <me@yannsainson.com>
2021-06-16 09:20:03 +0200

    Fix server https

--- services/api/client.ts
+++ services/api/client.ts
@@ -2,7 +2,9 @@ import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: 'http://api.hiddentity.fr/query',
+  uri: __DEV__
+    ? 'http://api.hiddentity.fr/query'
+    : 'https://hazel.herokuapp.com/query',
 });
 
 const authLink = setContext((_, context) => {
